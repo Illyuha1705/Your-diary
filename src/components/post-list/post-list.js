17b0b57149d571cb3aeba12c58789b6f0f01e788
Illyuha1705 <75776668+Illyuha1705@users.noreply.github.com>
2021-03-12 17:22:57 +0300

    Added 2 features: star and heart

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,12 +4,18 @@ import PostListItem from '../post-list-item/post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key={item.id} className="list-group-item">
+                <PostListItem {...itemProps}/>
+            </li>
+        )
+    });
     return (
         <ul className="app-list list-group">
-            <PostListItem label="Going to learn React"/>
-            <PostListItem label="That is so good"/>
-            <PostListItem label="I need a break"/>
+            {elements}
         </ul>
     )
 }
